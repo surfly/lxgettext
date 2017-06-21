845fb9580c5db0347b218c03f3de8d777eb35f2c
Yauhen Shulitski <jsnjack@gmail.com>
2017-06-21 12:34:11 +0200

    Add tests

---
+++ test_input.py
@@ -0,0 +1,40 @@
+import unittest
+
+from lxgettext import generate_po
+
+
+class TestInputData(unittest.TestCase):
+    def setUp(self):
+        class Args:
+            output = None
+            path = "__init__.py"
+
+        self.args = Args()
+
+    def test_lonely_warrior_dq(self):
+        data = """gettext("Warrior")"""
+        expected = 'msgid "Warrior"'
+        result = generate_po(data, self.args)
+        self.assertIn(expected, result)
+
+    def test_lonely_warrior_sq(self):
+        data = """gettext('Warrior')"""
+        expected = 'msgid "Warrior"'
+        result = generate_po(data, self.args)
+        self.assertIn(expected, result)
+
+    def test_sentence(self):
+        data = """gettext('I want ice cream')"""
+        expected = 'msgid "I want ice cream"'
+        result = generate_po(data, self.args)
+        self.assertIn(expected, result)
+
+    def test_quotes(self):
+        data = """gettext('H"O"T')"""
+        expected = 'msgid "H"O"T"'
+        result = generate_po(data, self.args)
+        self.assertIn(expected, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
