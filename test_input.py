22c9d8946769c450167340569cb02e22ed20936e
Yauhen Shulitski <jsnjack@gmail.com>
2017-06-22 15:47:05 +0200

    Update regexp

--- test_input.py
+++ test_input.py
@@ -3,7 +3,7 @@ import unittest
 from lxgettext import generate_po
 
 
-class TestInputData(unittest.TestCase):
+class TestInput(unittest.TestCase):
     def setUp(self):
         class Args:
             output = None
@@ -34,6 +34,49 @@ class TestInputData(unittest.TestCase):
         expected = 'msgid "H"O"T"'
         result = generate_po(data, self.args.path, self.args)
         self.assertIn(expected, result)
+
+    def test_widget(self):
+        data = """blockPage(Surfly.i18n.gettext('Cobrowsing session is opened in a separate tab.') + " " +"""
+        expected = 'msgid "Cobrowsing session is opened in a separate tab."'
+        result = generate_po(data, self.args.path, self.args)
+        self.assertIn(expected, result)
+
+    def test_widget2(self):
+        data = """
+                blockPage(Surfly.i18n.gettext('Cobrowsing session is opened in a separate tab.') + " " +
+                Surfly.i18n.gettext('Please do not close this window while it is active.'),
+        """
+        result = generate_po(data, self.args.path, self.args)
+        self.assertIn('msgid "Cobrowsing session is opened in a separate tab."', result)
+        self.assertIn('msgid "Please do not close this window while it is active."', result)
+
+    def test_double(self):
+        data = """
+                gettext("Lonely")
+                gettext("Warrior")
+        """
+        result = generate_po(data, self.args.path, self.args)
+        self.assertIn('msgid "Warrior"', result)
+        self.assertIn('msgid "Lonely"', result)
+
+    def test_double2(self):
+        data = """
+                :aria-label="open.publishing ? $gettext('Leave videochat') : $gettext('Join videochat')">
+        """
+        result = generate_po(data, self.args.path, self.args)
+        self.assertIn('msgid "Leave videochat"', result)
+        self.assertIn('msgid "Join videochat"', result)
+
+    def test_double_string(self):
+        data = """
+                if (!this.options.ui_off && !this.$store.state.delayedEnd) {
+                    let msg = this.$gettext('You have an active cobrowsing session, are you sure you want to close this window?');
+                    e.returnValue = msg;
+                    return msg;
+                }
+        """
+        result = generate_po(data, self.args.path, self.args)
+        self.assertIn('msgid "You have an active cobrowsing session, are you sure you want to close this window?"', result)
 
 
 if __name__ == '__main__':
