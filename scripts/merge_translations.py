94c4c7c89f2f9cad5ebdb5349500410e77ddb3a6
Quint Guvernator <quint@guvernator.net>
2019-09-09 22:49:06 +0200

    fix msgctxt bug

--- scripts/merge_translations.py
+++ scripts/merge_translations.py
@@ -10,6 +10,7 @@ from functools import reduce
 from operator import iconcat
 from pathlib import Path
 from sys import argv, exit
+from collections import Counter
 
 default_metadata = {
     "Project-Id-Version": "PACKAGE VERSION",
@@ -25,30 +26,9 @@ default_metadata = {
 }
 
 
-class Stats():
-    def __init__(self):
-        self.empty_merges = 0
-        self.trivial_merges = 0
-        self.conflict_merges = 0
-
-    def __str__(self):
-        w = max(len(str(x)) for x in (self.empty_merges, self.trivial_merges, self.conflict_merges))
-        return '\n'.join([
-            f"Strings with no translations: {self.empty_merges:{w}}",
-            f"Strings with one translation: {self.trivial_merges:{w}}",
-            f"Strings with > 1 translation: {self.conflict_merges:{w}}",
-        ])
-
-    def __add__(self, other):
-        ret = self.__class__()
-        ret.empty_merges = self.empty_merges + other.empty_merges
-        ret.trivial_merges = self.trivial_merges + other.trivial_merges
-        ret.conflict_merges = self.conflict_merges + other.conflict_merges
-        return ret
-
-
 def combine_strings(strings: [str]) -> str or None:
-    return '\n'.join(s for s in strings if s)
+    strings = [ s for s in strings if s ]
+    return '\n'.join(strings) if strings else None
 
 
 def combine_flags(flag_lists: [str]) -> list([str]):
@@ -115,48 +95,70 @@ def choose_translation(entries: [polib.POEntry], plural=False, stats=None) -> st
     # trivial cases
     if len(translations) == 0:
         if stats:
-            stats.empty_merges += 1
+            stats['empty_merges'] += 1
         return ''
     elif len(translations) == 1:
         if stats:
-            stats.trivial_merges += 1
+            stats['trivial_merges'] += 1
         return translations[0]
 
     # if there's more than one unique translation, we must consult the user
     translation = choose_string('translation', entries[0].msgid, translations)
     if stats:
-        stats.conflict_merges += 1
+        stats['conflict_merges'] += 1
     return translation
+
+
+def filter_poentries(entries: [polib.POEntry], stats=None) -> [polib.POEntry]:
+    for entry in entries:
+
+        # TODO: implement merging plural forms
+        if entry.msgid_plural:
+            if stats:
+                if 'plurals' not in stats:
+                    print(f"Merging plural forms is not implemented yet! Skipping entry... (e.g. msgid \"{entry.msgid}\" in {po_file.fpath})")
+                stats['plurals'] += 1
+            continue
+
+        # TODO: implement merging msgctxt variants
+        if entry.msgctxt is not None:
+            if stats:
+                if 'msgctxt' not in stats:
+                    print(f"Merging msgctxt variants is not implemented yet! (e.g. msgid \"{entry.msgid}\" in {po_file.fpath})")
+                stats['msgctxt'] += 1
+            entry.msgctxt = None
+
+        yield entry
 
 
 def combine_poentries(entries: list([polib.POEntry]), stats=None) -> polib.POEntry or None:
     '''Might require user input in non-trivial cases!'''
 
-    # trivial cases
-    assert(entries)
+    assert entries
+
     if len(entries) == 1:
         # caution: this is not a new object!
         if stats:
-            stats.trivial_merges += 1
+            stats['trivial_merges'] += 1
         return entries[0]
 
     # DEBUG msgids have to be the same
-    assert(all(entries[0].msgid == e.msgid for e in entries))
-    assert(all(entries[0].msgid_plural == e.msgid_plural for e in entries))
+    assert all(entries[0].msgid == e.msgid for e in entries)
+    assert all(entries[0].msgid_plural == e.msgid_plural for e in entries)
 
     return polib.POEntry(
         msgid=entries[0].msgid,
-        # msgid_plural=
+        # msgid_plural= # TODO
         msgstr=choose_translation(entries, stats=stats),
-        # msgstr_plural=
-        msgctxt=combine_strings(e.msgctxt for e in entries),
+        # msgstr_plural= # TODO
+        # msgctxt= # TODO
         obsolete=all(e.obsolete for e in entries),
-        # encoding=
+        # encoding= # TODO
         comment=combine_strings(e.comment for e in entries),
         tcomment=combine_strings(e.tcomment for e in entries),
         occurrences=reduce(iconcat, (e.occurrences for e in entries), []),
         flags=combine_flags(e.flags for e in entries),
-        previous_msgctxt=combine_strings(e.previous_msgctxt for e in entries),
+        #previous_msgctxt= # TODO
         previous_msgid=combine_strings(e.previous_msgid for e in entries),
         previous_msgid_plural=combine_strings(e.previous_msgid_plural for e in entries),
         # linenum=
@@ -165,7 +167,7 @@ def combine_poentries(entries: list([polib.POEntry]), stats=None) -> polib.POEnt
 
 def combine_headers(po_files: [polib.POFile], language=None) -> polib.POFile:
     '''Combines the headers of several PO files into a new, empty PO file.'''
-    ret = polib.POFile()
+    ret = polib.POFile(wrapwidth=0)
 
     # populate with default metadata
     ret.metadata.update(default_metadata)
@@ -194,15 +196,7 @@ def combine_pofiles(po_files: list([polib.POFile]), language=None, stats=None) -
 
     translations = dict()
     for po_file in po_files:
-        for entry in po_file:
-
-            # FIXME: implement merging plural forms
-            if entry.msgid_plural:
-                raise NotImplementedError(
-                    "Merging plural forms is not implemented yet."
-                    + f' Offending entry is in {po_file.fpath} under msgid "{entry.msgid}".'
-                )
-
+        for entry in filter_poentries(po_file, stats=stats):
             try:
                 translations[entry.msgid].append(entry)
             except KeyError:
@@ -248,10 +242,10 @@ if __name__ == "__main__":
     try:
         languages = get_languages(locale_paths)
 
-        totals = Stats()
+        totals = Counter()
         outputs = dict()
         for language in languages:
-            langstat = Stats()
+            langstat = Counter()
 
             po_paths = [f / language / 'LC_MESSAGES' / 'django.po' for f in locale_paths]
             po_files = [polib.pofile(str(p)) for p in po_paths if p.exists()]
